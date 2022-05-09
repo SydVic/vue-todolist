e719b6e96469f84988d86c1c57892b53c0826173
SydVic <fabrizio.bocca27@gmail.com>
2022-05-09 14:02:19 +0200

    aggiunto array oggetti to do list

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@ const app = new Vue (
     {
         el: "#root",
         data: {
-
+            toDoList: [
+                {
+                    text: "pulire casa",
+                    done: false,
+                },
+                {
+                    text: "dare da mangiare ai pesci",
+                    done: false,
+                },
+                {
+                    text: "ritirare macchina dal meccanico",
+                    done: false,
+                },
+                {
+                    text: "controllare asta ebay",
+                    done: false,
+                },
+            ],
         },
         methods: {
 
